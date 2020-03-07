f6bda675a0549300cf4cd2dd89ad7cdb28a1347c
Patricio Reyes <reyes_p23@hotmail.com>
2020-03-06 19:13:32 -0600

    Add files via upload

--- Graphs/Graphs/PRProgressView.swift
+++ Graphs/Graphs/PRProgressView.swift
@@ -1,16 +1,28 @@
-//
-//  PRProgressView.swift
-//  Graphs
-//
-//  Created by Patricio on 06/03/20.
-//  Copyright © 2020 patricioreyes. All rights reserved.
-//
+/*
+The MIT License (MIT)
+Copyright (c) 2015 Max Konovalov
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 import UIKit
 
 class PRProgressView: UIView {
     
-    public var innerDiameter: CGFloat = 170
+    public var strokeWidth: CGFloat = 15
     public var strokeColor: UIColor = UIColor(red:0.24, green:0.87, blue:0.59, alpha:1.0)
     public var baseColor: UIColor = UIColor.black.withAlphaComponent(0.05)
     public var originPosition: PRProgressViewOriginPosition = .top
@@ -25,7 +37,7 @@ class PRProgressView: UIView {
         baseShape.path = UIBezierPath(arcCenter: CGPoint(x: radius, y: radius), radius: radius, startAngle: originPosition.startAngle, endAngle: originPosition.endAngle, clockwise: true).cgPath
         baseShape.position = CGPoint(x: radius, y: radius)
         baseShape.strokeColor = baseColor.cgColor
-        baseShape.lineWidth = (self.bounds.size.width - innerDiameter)/2
+        baseShape.lineWidth = strokeWidth
         baseShape.fillColor = UIColor.clear.cgColor
         self.layer.addSublayer(baseShape)
         
--- Graphs/Graphs/ViewController.swift
+++ Graphs/Graphs/ViewController.swift
@@ -10,18 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var progressView: PRProgressView!
+    @IBOutlet weak var progressView: PRProgressView!{
+        didSet{
+            progressView.strokeColor = UIColor(red:0.88, green:0.01, blue:0.10, alpha:1.0)
+            progressView.baseColor = UIColor(red:0.88, green:0.01, blue:0.10, alpha: 0.15)
+        }
+    }
+    
+    @IBOutlet weak var middleProgressView: PRProgressView!{
+        didSet{
+            middleProgressView.strokeColor = UIColor(red:0.27, green:0.88, blue:0.04, alpha:1.0)
+            middleProgressView.baseColor = UIColor(red:0.27, green:0.88, blue:0.04, alpha:0.15)
+        }
+    }
+    
+    @IBOutlet weak var insideProgressView: PRProgressView!{
+        didSet{
+            insideProgressView.strokeColor = UIColor(red:0.17, green:0.75, blue:0.88, alpha:1.0)
+            insideProgressView.baseColor = UIColor(red:0.17, green:0.75, blue:0.88, alpha:0.15)
+        }
+    }
+    
     @IBOutlet weak var progressSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let value = CGFloat(progressSlider?.value ?? 0)
-        progressView.setProgress(value: value)
+        setInitialState()
     }
 
     @IBAction func animateButtonPressed(_ sender: Any) {
-        let value = CGFloat(progressSlider?.value ?? 0)
-        progressView.animate(to: value)
+        setInitialState()
+        progressView.animate(to: 70)
+        middleProgressView.animate(to: 60)
+        insideProgressView.animate(to: 40)
+    }
+    
+    func setInitialState(){
+        progressView.setProgress(value: 3)
+        middleProgressView.setProgress(value: 5)
+        insideProgressView.setProgress(value: 1)
     }
 }
-
